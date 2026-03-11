7adc31cf8a94ff9b35e9d94580b8f6cff3c516ae
Alexaa <afanaaseva.sasha.a@gmail.com>
2026-03-11 21:13:50 +0500

    Изменение формы

--- requests/forms.py
+++ requests/forms.py
@@ -6,13 +6,16 @@ from datetime import date
 class RequestForm(forms.ModelForm):
     class Meta:
         model = Request
-        fields = ['homeTechType', 'homeTechModel', 'problemDescryption', 'client', 'master']
+        fields = ['homeTechType', 'homeTechModel', 'problemDescryption', 
+                  'client', 'master', 'requestStatus', 'completionDate']  
         widgets = {
             'problemDescryption': forms.Textarea(attrs={'rows': 4, 'class': 'form-control', 'placeholder': 'Опишите проблему...'}),
             'homeTechType': forms.Select(attrs={'class': 'form-control'}),
             'homeTechModel': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Например: Indesit DS 316 W'}),
             'client': forms.Select(attrs={'class': 'form-control'}),
             'master': forms.Select(attrs={'class': 'form-control'}),
+            'requestStatus': forms.Select(attrs={'class': 'form-control'}),  
+            'completionDate': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),  
         }
     
     def __init__(self, *args, **kwargs):
@@ -20,10 +23,15 @@ class RequestForm(forms.ModelForm):
         self.fields['client'].queryset = User.objects.filter(user_type='Заказчик')
         self.fields['master'].queryset = User.objects.filter(user_type='Мастер')
         self.fields['master'].required = False
+        
+        # Настройка labels
         self.fields['client'].label = 'Клиент'
         self.fields['homeTechType'].label = 'Тип техники'
         self.fields['homeTechModel'].label = 'Модель'
         self.fields['problemDescryption'].label = 'Описание проблемы'
+        self.fields['requestStatus'].label = 'Статус заявки'
+        self.fields['completionDate'].label = 'Дата завершения'  
+        self.fields['completionDate'].required = False  
 
 
 class CommentForm(forms.ModelForm):
